82e1c406b1b4f2eed11bba8a2c1f7e0467f2fcb4
Krish Kumar Chaurasia <krishchaurasia890@gmail.com>
2026-04-11 23:16:58 +0530

    Update server.js

--- opssyn-backend/server.js
+++ opssyn-backend/server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(morgan('dev'));
-app.use('/api/webhooks', webhookRoutes);
+app.use('/api/webhooks/github', express.raw({ type: 'application/json' }));
 app.use(express.json());
 
 app.get('/health', (req, res) => {
