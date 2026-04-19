6445aaa8d892df3f03269e3cefbf591bf5365396
Krish Kumar Chaurasia <krishchaurasia890@gmail.com>
2026-04-20 01:36:28 +0530

    Update Incident.js

--- opssyn-backend/models/Incident.js
+++ opssyn-backend/models/Incident.js
@@ -33,7 +33,7 @@ const IncidentSchema = new mongoose.Schema(
     createdBy: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
-      required: true,
+      required: false,
     },
     resolvedAt: {
       type: Date,
